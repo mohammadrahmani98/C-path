537f90353757c07151a4e6b2c4d0f7abcbca1ddb
Mohammad Rahmani <mohammad.rahmani@studenti.polito.it>
2022-11-17 19:28:13 +0100

    Learn to Program with C++ 17
    Learn to Program with C++ 17
    Part 3 Stream IO and local Variables

---
+++ P1-Stream IO/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+// ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main()
+{
+	cout << 34 << '\n';
+	cout << "Hey darlin" << 'n';
+	// Is just to see soth out on the screen 
+
+	cout << '\n' << '\n';
+	int i = 2 + 2 << '\n';
+	cout << i << '\n';
+	i = i * 3;
+	cout << i << '\n';
+
+	cout << "Enter a number:";
+	cin >> i;
+	cout << '\n' << "Here what you get if we double it:" << '\n' << '\n' << i * 2;
+
+
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
