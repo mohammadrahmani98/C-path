65c302bc95374f402936a37352cc93059a0dae1c
Mohammad Rahmani <100317203+mohammadrahmani98@users.noreply.github.com>
2022-11-20 15:38:26 +0100

    next update

--- P2-Flow of Control/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ P2-Flow of Control/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -10,24 +10,42 @@ int main()
 {
     int firsnumber;
     int secondnumber;
-    cout << "Enter your fucking number:\n";
-    cin >> firsnumber;
-    cout << "You entered this number, " << firsnumber << "\nEnter another number\n";
-    cin >> secondnumber;
-
-    if (firsnumber < secondnumber)
+    bool keepgoing = true;
+    int answer;
+    while(keepgoing)
     {
-        cout << "the first number," << firsnumber << ", is less than the second number" << secondnumber << '\n';
-     }
-    if( firsnumber==secondnumber)
-    { 
-        cout << "the first number, " << firsnumber << " ,is equal to second number" << secondnumber << '\n';
-    }
-    if (firsnumber > secondnumber)
-    {
-        cout << "The first number is," << firsnumber << " ,is greater than the second number, " << secondnumber<< '\n';
+        cout << "Enter your fucking number:\n";
+        cin >> firsnumber;
+        cout << "You entered this number, " << firsnumber << "\nEnter another number\n";
+        cin >> secondnumber;
 
+        if (firsnumber < secondnumber)
+        {
+            cout << "the first number," << firsnumber << ", is less than the second number" << secondnumber << '\n';
+        }
+        else
+        {
+            cout << "the first number," << firsnumber << ", is not less than the second number" << secondnumber << '\n';
+        }
+        if (firsnumber == secondnumber)
+        {
+            cout << "the first number, " << firsnumber << " ,is equal to second number" << secondnumber << '\n';
+        }
+        if (firsnumber > secondnumber)
+        {
+            cout << "The first number is," << firsnumber << " ,is greater than the second number, " << secondnumber << '\n';
+        }
+        for (int loop = 0; loop < 10 ; loop++ )
+        {
+            cout << loop << "";
 
+        }
+        cout << "\n Hey bro, you still here? if you want to stop put 0 or any other number to continue:\n";
+        cin>> answer;
+        if (answer == 0)
+        {
+            keepgoing = false;
+        }
     }
     system("pause");
 }
